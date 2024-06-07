fa23066f00f1fb9d0fb6390c750396d00b35af40
Stefan <stefan@razorsecure.com>
2024-06-07 23:02:54 +0100

    Creating separate class for external API.

---
+++ src/main/java/com/stefan/propertyviewer/GeoApifyApi.java
@@ -0,0 +1,62 @@
+package com.stefan.propertyviewer;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.stefan.propertyviewer.entity.Country;
+
+@Component
+public class GeoApifyApi {
+
+    
+    private static String apiKey;
+
+
+    public static String getApiGet() {
+        return apiKey;
+    }
+
+    @Value("${geoapify.key}")
+    public void setApiGey(String apiKey) {
+        GeoApifyApi.apiKey = apiKey;
+    }
+
+    public static Double[] getCoordinatesFromGeoApi(String buildingName, String city, String postcode, Country country) {
+        String addressLine = URLEncoder.encode(buildingName + " " + city + " " + postcode + " " + country.toString().replace("_"," "), StandardCharsets.UTF_8);
+        String urlString = "https://api.geoapify.com/v1/geocode/search?text="+ addressLine + "&apiKey=" + apiKey;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+        .uri(URI.create(urlString))
+        .header("Content-Type", "application/json")
+        .build();
+        HttpResponse<String> response;
+        try {
+            response = client.send(request, BodyHandlers.ofString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return null;
+        }
+        JSONObject jsonObj =  new JSONObject(response.body());
+        JSONArray arrayObject = jsonObj.getJSONArray("features");
+        JSONObject featuresObject = arrayObject.getJSONObject(0).getJSONObject("properties");
+        Double[] coordinates = new Double[2];
+        coordinates[0] = featuresObject.getDouble("lat");
+        coordinates[1] = featuresObject.getDouble("lon");  
+        return coordinates;
+    }
+
+    
+}
--- src/main/java/com/stefan/propertyviewer/entity/Property.java
+++ src/main/java/com/stefan/propertyviewer/entity/Property.java
@@ -16,7 +16,10 @@ import java.nio.charset.StandardCharsets;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.json.JSONArray;
+import org.springframework.stereotype.Component;
+import com.stefan.propertyviewer.GeoApifyApi;
 
 @Entity
 public class Property {
@@ -40,6 +43,7 @@ public class Property {
         this.postcode = postcode;
         this.country = Country.valueOf(country);
         try{
+
             this.getCoordinates();
         }
         catch (Exception e){
@@ -81,21 +85,7 @@ public class Property {
         if (this.coordinates != null){
             return this.coordinates;
         }
-        String addressLine = URLEncoder.encode(buildingName + " " + city + " " + postcode + " " + country.toString().replace("_"," "), StandardCharsets.UTF_8);
-        String urlString = "https://api.geoapify.com/v1/geocode/search?text="+ addressLine + "&apiKey=2b6155e267ec459a9f4197eec515cde1";
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(urlString))
-        .header("Content-Type", "application/json")
-        .build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        JSONObject jsonObj =  new JSONObject(response.body());
-        JSONArray arrayObject = jsonObj.getJSONArray("features");
-        JSONObject featuresObject = arrayObject.getJSONObject(0).getJSONObject("properties");
-        Double[] coordinates = new Double[2];
-        coordinates[0] = featuresObject.getDouble("lat");
-        coordinates[1] = featuresObject.getDouble("lon");  
-        return coordinates;
+        return GeoApifyApi.getCoordinatesFromGeoApi(buildingName,city,postcode,country);
     }
 
     public void setCoordinates(Double[] coordinates){
